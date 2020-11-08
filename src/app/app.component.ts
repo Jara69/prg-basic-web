469b60fb3a3ee368c134a8297b744ed972ede36e
jaroslav <jaroslav.havrda@educanet.cz>
2020-11-08 17:43:36 +0100

    idk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+class SearchOption {
+title: string;
+path: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-web';
+  search = '';
+  block = false;
+  options: SearchOption[] = [
+    {title: 'Home', path: 'home'},
+    {title: 'Contact', path: 'contact'},
+    {title: 'About', path: 'about'}
+  ];
+
+
+  get filteredOptions(): SearchOption[]{
+    const result = [];
+    const search = this.search.toLowerCase();
+    if (search.length === 0){
+      return this.options;
+    }
+    for (const option of this.options){
+      if (option.title.toLowerCase().includes(search)){
+        result.push(option);
+      }
+    }
+    return result;
+  }
+
+  toggleOptions() {
+    if (this.block === true) {
+      this.block = false;
+    } else {
+      this.block = true;
+    }
+  }
+
+  toggleOptionsOff() {
+    this.block = false;
+  }
+
 }
